31706ad7d3fd8d0c99ff39bcac72007f48987320
sleepyasterin-design <sleepyasterin@gmail.com>
2025-09-10 19:10:46 +0530

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+module.exports = db;
